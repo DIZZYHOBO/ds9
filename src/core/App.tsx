ecfcf5a2b9cd456eae09ae3a8f798ef193662aea
DIZZYHOBO <dystorp@gmail.com>
2026-01-04 17:55:46 +0800

    Update App.tsx

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -11,6 +11,7 @@ import "@ionic/react/css/float-elements.css";
 import "@ionic/react/css/padding.css";
 import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
+import "./compact-header.css";
 
 // Override Ionic variables
 import "./theme/variables.css";
